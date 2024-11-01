d69b107553c498454ff7c346883aaf2702996d41
Marius1608 <panteamarius1000@gmail.com>
2024-11-01 17:45:12 +0200

    Commit

--- expense-tracker/src/main/java/com/expensemanagement/expense_tracker/ExpenseTrackerApplication.java
+++ expense-tracker/src/main/java/com/expensemanagement/expense_tracker/ExpenseTrackerApplication.java
@@ -3,6 +3,13 @@ package com.expensemanagement.expense_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping
+@CrossOrigin("http://localhost:3000/")
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
--- expense-tracker/src/test/java/com/expensemanagement/expense_tracker/ExpenseTrackerApplicationTests.java
+++ expense-tracker/src/test/java/com/expensemanagement/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -2,13 +2,7 @@ package com.expensemanagement.expense_tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@CrossOrigin("http://localhost:3000/")
-//@RequestMapping("/api/v1")
 @SpringBootTest
 class ExpenseTrackerApplicationTests {
 
